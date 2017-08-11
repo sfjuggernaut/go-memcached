cebc51da8904953b62899e5519d94d3772d0da24
eric kustarz <sfjuggernaut@gmail.com>
2017-08-11 16:40:49 -0700

    Update main cmd to pass along proper args for connection control

--- cmd/go-memcached/main.go
+++ cmd/go-memcached/main.go
@@ -8,10 +8,12 @@ import (
 
 var port = flag.Int("port", 11211, "port to run memcached server")
 var size = flag.Int("size", 1024, "maximum number of entries to store")
+var numWorkers = flag.Int("num-workers", 8, "number of workers to process incoming connections")
+var maxNumConnections = flag.Int("max-num-connections", 1024, "maximum number of simultaneous connections")
 
 func main() {
 	flag.Parse()
 
-	server := server.New(*port, *size)
+	server := server.New(*port, *size, *numWorkers, *maxNumConnections)
 	server.Start()
 }
